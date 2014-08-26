932957ad37fe56a22111780309bf484f4ada33e5
Gai Takakura <gixa.aturakka@gmail.com>
2014-08-26 19:54:03 +0900

    問題解決(仮)

---
+++ Assets/Scripts/CheckVelocity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckVelocity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.velocity = new Vector3(3, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//rigidbody.velocity = new Vector3(30, 0, 0);
+		Debug.Log(rigidbody.velocity);
+	}
+}
--- Assets/Scripts/Player/ControlPlayer.cs
+++ Assets/Scripts/Player/ControlPlayer.cs
@@ -55,25 +55,33 @@ public class ControlPlayer : MonoBehaviour {
 	//------------------------------
 	// 移動
 	private void Move(){
-
 		dir = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0.0f, Input.GetAxisRaw ("Vertical")).normalized;	// 入力中の角度
 		rot = Mathf.Atan2 (dir.x, dir.z) * Mathf.Rad2Deg + this.transform.localEulerAngles.y;					// 入力 + 現在向いている角度
 		if (rot > 360.0f) rot -= 360.0f;	if (rot < 0.0f) rot += 360.0f;										// 角度補正
+		Vector3 Vec = new Vector3(0,0,0);
 		SpeedSet = 1.0f;
 		if (dir == new Vector3 (0.0f, 0.0f, 0.0f)) {
 			if(jump) SpeedSet = 0.0f;
 			else SpeedSet = 0.0f;
 		}
-		Vector3 Vec = new Vector3(0,0,0);// = new Vector3 ((float)Math.Sin (rot * Math.PI / 180) * Speed * SpeedSet, rigidbody.velocity.y, (float)Math.Cos (rot * Math.PI / 180) * Speed * SpeedSet);
-		if(jump == false) {
-			Vec = new Vector3 ((float)Math.Sin (rot * Math.PI / 180) * Speed * SpeedSet, rigidbody.velocity.y, (float)Math.Cos (rot * Math.PI / 180) * Speed * SpeedSet);;
-			rigidbody.velocity = Vec;
-		}
 
-		// ジャンプ
-		if (Input.GetButtonDown ("Jump") && jump == false){
-			rigidbody.velocity = new Vector3 (rigidbody.velocity.x, 3.0f, rigidbody.velocity.z);
-			jump = true;
+		// 分岐　親がある or 親がない
+		if(this.gameObject.transform.parent == null){
+			// 移動
+			if(jump == false) {
+				Vec = new Vector3 ((float)Math.Sin (rot * Math.PI / 180) * Speed * SpeedSet, rigidbody.velocity.y, (float)Math.Cos (rot * Math.PI / 180) * Speed * SpeedSet);
+				rigidbody.velocity = Vec;
+			}
+
+			// ジャンプ
+			if (Input.GetButtonDown ("Jump") && jump == false){
+				rigidbody.velocity = new Vector3 (rigidbody.velocity.x, 3.0f, rigidbody.velocity.z);
+				jump = true;
+			}
+		}else{
+			Vec = new Vector3 ((float)Math.Sin (rot * Math.PI / 180) * Speed * SpeedSet, rigidbody.velocity.y, (float)Math.Cos (rot * Math.PI / 180) * Speed * SpeedSet);
+			this.gameObject.transform.parent.rigidbody.velocity = Vec;
+			//Debug.Log(Vec);
 		}
 	}
 }
